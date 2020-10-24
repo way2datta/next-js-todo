ce1cb1c1476ad1a05cade29ebb72767a8d37405c
Dattatray Kale <way2datta@gmail.com>
2020-10-24 20:08:23 +0530

    Circle ci

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './_shared.scss';
 
-// eslint-disable-next-line react/prop-types
 export default function MyApp({ Component, pageProps }) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />;
 }
